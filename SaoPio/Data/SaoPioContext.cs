e6556ad07f4e85aba1add282865b415376bc57dd
eddyjunior <neggoh@gmail.com>
2020-01-08 18:35:36 -0200

    sao-pio-rogai-por-nos

---
+++ SaoPio/Data/SaoPioContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaoPio.Models
+{
+    public class SaoPioContext : DbContext
+    {
+        public SaoPioContext (DbContextOptions<SaoPioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord>  SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+    }
+}
--- SaoPio/Models/ViewModels/ErrorViewModel.cs
+++ SaoPio/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SaoPio.Models
+namespace SaoPio.Models.ViewModels
 {
     public class ErrorViewModel
     {
